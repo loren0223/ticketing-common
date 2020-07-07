555467680468a99d42b5b7ed71de6aeeff653d91
LorenCheng <loren@agreeinfotech.com>
2020-07-07 21:48:36 +0800

    add new event types for Order Service

--- src/events/types/order-status.ts
+++ src/events/types/order-status.ts
@@ -1,6 +1,6 @@
 export enum OrderStatus {
-  Created = 'order:created',
-  Cancelled = 'order:cancelled',
-  AwaitPayment = 'order:awaitPayment',
-  Completed = 'order:completed',
+  Created = 'created',
+  Cancelled = 'cancelled',
+  AwaitPayment = 'awaitPayment',
+  Completed = 'completed',
 }
